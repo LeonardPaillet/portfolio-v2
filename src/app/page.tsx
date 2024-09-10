154b1058686bb90b275fe0181c0ac41c255d3b13
LeonardPaillet <66920564+LeonardPaillet@users.noreply.github.com>
2024-09-10 19:38:47 +0200

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,17 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
+         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
           priority
-        />
+        />  
+
+
+
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
