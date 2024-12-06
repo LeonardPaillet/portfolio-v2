42c0100ee1e99e190b8af2dafef42cf43beb12d4
LeonardPaillet <66920564+LeonardPaillet@users.noreply.github.com>
2024-12-06 15:32:40 +0100

    header fini

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,30 @@
 export default function Header(){
   return(
-    <header className="bg-blue flex justify-between items-center py-4 px-24">
-      <div>
+    <header className="bg-blue flex justify-between items-center py-8 px-24 font-semibold text-md">
+      <a href="/" className="flex gap-4 items-center">
+        <img src="./logo.svg" alt="Logo" width={"20%"}/>
         <p className="uppercase">Léonard Paillet</p>
-      </div>
-      <div className="flex gap-24 font-bold text-md">
-        <a href="#">ACCUEIL</a>
-        <a href="#">PROJETS</a>
-        <a href="#">FORMATION</a>
-        <a href="#">CONTACT</a>
+      </a>
+      <div className="flex gap-24 ">
+        <HeaderLinks name="ACCUEIL" url="/"/>
+        <HeaderLinks name="PROJETS" url="/projects"/>
+        <HeaderLinks name="FORMATION" url="/experiences"/>
+        <HeaderLinks name="CONTACT" url="/contact"/>
       </div>
     </header>
+  )
+}
+
+interface HeaderLinkProps{
+  url : string;
+  name : string;
+}
+
+function HeaderLinks({url, name}: HeaderLinkProps){
+  return(
+    <a href={url} className="group hover:text-lightBlue duration-300">
+      {name}
+      <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-lightBlue"></span>
+    </a>
   )
 }
