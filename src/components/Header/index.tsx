871c89c4eeaa46aca5107ffa104f314817e1a9da
LeonardPaillet <66920564+LeonardPaillet@users.noreply.github.com>
2024-12-06 15:13:21 +0100

    new favicon

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,10 @@
 export default function Header(){
   return(
-    <header className="bg-blue flex justify-between items-center py-4 px-24 fixed z-20">
+    <header className="bg-blue flex justify-between items-center py-4 px-24">
       <div>
         <p className="uppercase">Léonard Paillet</p>
       </div>
-      <div className="flex gap-12 font-bold text-lg">
+      <div className="flex gap-24 font-bold text-md">
         <a href="#">ACCUEIL</a>
         <a href="#">PROJETS</a>
         <a href="#">FORMATION</a>
