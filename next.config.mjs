4533c6f502e43003a5ab657cd0e7c622938513c7
LeonardPaillet <66920564+LeonardPaillet@users.noreply.github.com>
2025-01-04 15:15:35 +0100

    config image host name

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "prod-files-secure.s3.us-west-2.amazonaws.com", // Domaine des images Notion
+        port: "",
+        pathname: "**", // Autoriser toutes les sous-routes
+      },
+    ],
+  },
+};
 
 export default nextConfig;
